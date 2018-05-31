b327fff06c0a9cd299f4f6162ef493a56a5d6cd8
Simon R Jones <simon@studio24.net>
2018-05-31 09:25:28 +0100

    Add further checks to make code more robust

--- wp-config.load.php
+++ wp-config.load.php
@@ -17,7 +17,7 @@ function s24_load_environment_config() {
     global $argv;
 
     // Set env if set via environment variable
-    if (getenv('WP_ENV') !== false) {
+    if (getenv('WP_ENV') !== false && !empty(getenv('WP_ENV'))) {
         define('WP_ENV', preg_replace('/[^a-z]/', '', getenv('WP_ENV')));
     }
 
@@ -33,7 +33,10 @@ function s24_load_environment_config() {
         if (!defined('WP_ENV')) {
             if (file_exists(__DIR__ . '/.env')) {
                 $environment = trim(file_get_contents(__DIR__ . '/.env'));
-                define('WP_ENV', preg_replace('/[^a-z]/', '', $environment));
+                $value = preg_replace('/[^a-z]/', '', $environment);
+                if (!empty($value)) {
+                    define('WP_ENV', preg_replace('/[^a-z]/', '', $environment));
+                }
             }
         }
     }
@@ -41,7 +44,7 @@ function s24_load_environment_config() {
     // Define site host
     if (isset($_SERVER['HTTP_X_FORWARDED_HOST']) && !empty($_SERVER['HTTP_X_FORWARDED_HOST'])) {
         $hostname = strtolower(filter_var($_SERVER['HTTP_X_FORWARDED_HOST'], FILTER_SANITIZE_STRING));
-    } else {
+    } elseif (isset($_SERVER['HTTP_HOST'])) {
         $hostname = strtolower(filter_var($_SERVER['HTTP_HOST'], FILTER_SANITIZE_STRING));
     }
 
@@ -57,8 +60,12 @@ function s24_load_environment_config() {
      * via the CLI for example) then get the Hostname using the WP_ENV environment
      * variable
      */
-    if(empty($hostname)) {
+    if (empty($hostname)) {
         $hostname = $env[WP_ENV]['domain'];
+    }
+
+    if (empty($hostname)) {
+        throw new Exception("Cannot determine current WordPress domain");
     }
 
     foreach ($env as $environment => $env_vars) {
--- wp-config.local.php
+++ wp-config.local.php
@@ -2,6 +2,10 @@
 /**
  * Local settings, you should keep these outside of version control
  *
+ * Please note this file is loaded first, then the environment config is loaded.
+ * Don't duplicate any WP config settings in this file and the environment file
+ * since WordPress uses constants and you'll raise a PHP notice error.
+ *
  * Enter any WordPress config settings that are specific to the local environment
  * in this file.
  *
